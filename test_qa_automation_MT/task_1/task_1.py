7341c272ccaa431332912481c07a6a6030762fe8
lambotik <lambotik1988@gmail.com>
2023-02-11 19:11:19 +0300

    Update: task1 Complete

---
+++ test_qa_automation_MT/task_1/task_1.py
@@ -0,0 +1,48 @@
+id = 0
+
+
+def get_id():
+    global id
+    id = id + 1
+    return id
+
+
+class Vehicle(object):
+    id = id
+
+    def __new__(cls):
+        cls.id = get_id()
+        return cls
+
+
+class Tank(Vehicle):
+    def __init__(self):
+        object_id_collector = self.id
+
+
+class Tankman(Vehicle):
+    uid = Vehicle.id
+
+    def __new__(cls):
+        cls.id = get_id()
+        return cls
+
+
+class TankCommander(Tankman):
+    def __init__(self):
+        object_id_collector = self.id
+
+
+class TankGunner(Tankman):
+    def __init__(self):
+        object_id_collector = self.id
+
+
+def check_object_id_collector():
+    expected_ids = (1, 2, 3, 4, 5)
+    actual_ids = (TankGunner().id, TankGunner().id, Tank().id, TankCommander().id, Tank().id)
+    assert actual_ids == expected_ids, 'Expected_ids: {}. Actual_ids: {}.'.format(expected_ids, actual_ids)
+    print 'Test passed. Amazing job!'
+
+
+check_object_id_collector()
