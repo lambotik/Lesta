fb7065fdbec2bd00730406ec4516d310580a55b6
lambotik <lambotik1988@gmail.com>
2023-02-15 14:23:36 +0300

    Update: Repair import problem

--- test_qa_automation_MT/task_3/logger/__init__.py
+++ test_qa_automation_MT/task_3/logger/__init__.py
@@ -1,11 +1,8 @@
-
-
-
 class fin_logger():
 
     def log_state(self, player):
         credit, gold, tank, turret = player.user_inventory()
-        print '\n', '#'*5, 'Player inventory', '#' * 5
+        print '\n', '#' * 5, 'Player inventory', '#' * 5
         print 'Player credits:', credit
         print 'Player gold:', gold
         print 'Player tank in hangar:', tank[0]
